f3a0972af17310ddb1525db017a716637a9f6c85
Eddie Hernando Jr <edzstrock@hotmail.com>
2018-11-14 05:07:51 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<title>Welcome to Doodle</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<head>
+<body>
+
+	<div class="wrapper indexPage">
+
+		<div class="mainSection">
+			
+			<div class="logoContainer">
+				<img src="assets/images/doodleLogo.png">
+			</div>
+
+			<div class="searchContainer" method="GET">
+
+				<form action="search.php">
+
+					<input class="searchBox" type="text"name="term">
+					<input class="searchButton" type="submit" value="Search">
+					
+				</form>
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
