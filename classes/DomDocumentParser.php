de2c5f66d63dff3b5eb4bb7ec1423186c3f3a576
Eddie Hernando Jr <edzstrock@hotmail.com>
2018-11-15 20:17:29 -0500

    Echoing the found urls

--- classes/DomDocumentParser.php
+++ classes/DomDocumentParser.php
@@ -9,13 +9,15 @@ class DomDocumentParser {
         $options = array(
             'http'=>array('method'=>"GET", 'header'=>"User-Agent: doodleBot/0.1\n")
             );
-        
         $context = stream_context_create($options);
 
-        $this->$doc = new DomDocument();
+        $this->doc = new DomDocument();
+        @$this->doc->loadHTML(file_get_contents($url, false, $context));
 
-        @$this->$doc->loadHTML(file_get_contents($url, false, $context));
+    }
 
+    public function getLinks() {
+        return $this->doc->getElementsByTagName("a");
     }
 }
 
