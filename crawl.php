13225f78a2a814b299beabdd36071709dcb8a535
Eddie Hernando Jr <edzstrock@hotmail.com>
2018-11-15 20:30:14 -0500

    Ignoring links  we don't want to use

--- crawl.php
+++ crawl.php
@@ -2,6 +2,10 @@
 
 include("classes/DomDocumentParser.php");
 
+function createLink($src, $url) {
+
+}
+
 function followLinks($url) {
 
     $parser = new DomDocumentParser($url);
@@ -10,11 +14,22 @@ function followLinks($url) {
 
     foreach($linkList as $link) {
         $href = $link->getAttribute("href");
-        echo $href . "<br>";
+        
+        if(strpos($href, "#") !== false) {
+            continue;
+        }
+        else if(substr($href, 0, 11) == "javascript:") {
+            continue;
+        }
+
+        createLink($href, $url);
+
+        //echo $href . "<br>";
+
     }
 }
 
-$startUrl = "http://www.reecekenney.com";
+$startUrl = "http://www.bbc.com";
 followLinks($startUrl);
 
 ?>
